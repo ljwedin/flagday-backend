a79fd9aaf0580d0165efadb0a0b5edf1b7153729
Lisa <ljwedin@gmail.com>
2022-07-30 15:19:19 +0200

    removes cors and tests insert user via postman

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,38 @@
 var express = require('express');
 var router = express.Router();
+const CryptoJS = require('crypto-js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource');
+});
+
+router.post('/new', function (req, res, next) {
+    const userPassword = req.body.password;
+    const encryptedUserPassword = CryptoJS.AES.encrypt(
+        userPassword,
+        process.env.SALT_KEY
+    ).toString();
+
+    req.app.locals.db
+        .collection('users')
+        .insertOne({
+            userName: req.body.userName,
+            password: encryptedUserPassword,
+        })
+        .then((result) => {
+            res.send(result);
+        });
+});
+
+router.post('/login', function (req, res, next) {
+    const userName = req.body.userName;
+    req.app.locals.db
+        .collection('users')
+        .findOne({ userName: userName })
+        .then((result) => {
+            res.send(result);
+        });
 });
 
 module.exports = router;
